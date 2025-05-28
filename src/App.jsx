45129a621291a220b3e4629a0e8cf8eda4749575
RevJuanma <88888570+RevJuanma@users.noreply.github.com>
2025-05-27 21:02:58 -0300

    feat: Se hizo la instalacion y setup del ruteo usando react-router, ademas de un componente layout para login y register

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ReactImg from './components/ReactImg'
+import ReactImg from './components/icons/ReactImg'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+// Auth
+import LoginScreen from "./Login/LoginScreen";
+import RegisterScreen from "./Register/RegisterScreen";
+
+// Home
+import App from "../App";
+
+export { LoginScreen, RegisterScreen, App as AppScreen };
