06382236e82be33b36a8913b6ea084a6a33266bb
guilleviera96 <guillefce096@gmail.com>
2025-05-30 18:50:16 -0300

    Creados los componentes del ticket, hice unos cambios de unificar el label e input en un solo componente llamado TextField para no tener tantos componentes a la hora de codificar, cree un archivo index.jsx para realizar exportaciones a otros archivos en los que vamos a utilizar los componentes creados

---
+++ src/components/TestForm.jsx
@@ -0,0 +1,51 @@
+import Container from './Container';
+import TextField from './TextField';
+import Button from './Button';
+
+/**
+ * Componente TestForm (lo cree para testear los componentes del ticket y mostrar algo al recargar la página, si quieren pueden borrarlo)
+ * @returns 
+ * Renderiza un form con los componentes creados en el ticket <PRO-9>
+ * Envia un console.log de los datos ingresados en los inputs
+ */
+
+
+export default function TestForm() {
+  // esta const es para manejar el evento del formulario
+  const handleSubmit = (evento) => {
+    //previene la recarga de la pagina (lo que usualmente hace un form html)
+    evento.preventDefault();
+    //creo una const de formdata para agregar los datos y luego debo crear un objeto para poder guardarlos
+    const formData = new FormData(evento.target)
+    const data = {
+      email: formData.get('email'),
+      password: formData.get('password'),
+    };
+    // el print de consola
+    console.log(data);
+  };
+
+  return (
+    //aca utilizo el component Container, TextField y Button.
+    <Container>
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto space-y-6">
+        <TextField
+          label="Your email"
+          type="email"
+          name="email"
+          placeholder="example@example.com"
+        />
+        <TextField
+          label="Your Password"
+          type="password"
+          name="password"
+          placeholder="********"
+        />
+        <Button 
+        type = "submit"
+        text="Log in" />
+      </form>
+    </Container>
+  );
+}
+
---
+++ src/components/TextField.jsx
@@ -0,0 +1,26 @@
+
+/**
+ * Componente TextField
+ * @returns 
+ * Renderiza:
+ * div con position flex column y un gap 2 para separar
+ * Etiqueta label para que el usuario pueda visualizar que datos ingresar en los Inputs, htmlFor da informacion a que input se refiere.
+ * Etiqueta Input con propo de tipo, nombre y un placeholder de ejemplo (perdon no estaba en el figma)
+ * Envia un console.log de los datos ingresados en los inputs
+ */
+
+export default function TextField({ label, type, name, placeholder }) {
+    return (
+        <div className="flex flex-col gap-2">
+            <label
+                htmlFor={name}
+                className="text-[#2C4692] text-sm font-medium"
+            >
+                {label}
+            </label>
+            <input type={type} name={name} placeholder={placeholder}
+                className="border-2 border-[#2C4692] rounded-[10px] px-4 py-3 focus:outline-none focus:ring-2 focus:ring-[#2C4692]"
+            />
+        </div>
+    );
+}
