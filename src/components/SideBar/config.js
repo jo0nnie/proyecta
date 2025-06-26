5c2b86f5165244669b243bf660c139004d440f12
FraGal05 <46552826@roquegonzalez.org>
2025-06-26 19:18:38 -0300

    fix: Componetizacion de SideBar

---
+++ src/components/SideBar/config.js
@@ -0,0 +1,7 @@
+export const config = [
+    {title: "Inicio", path: "/" },
+    {title: "Categorías", path: "/" },
+    {title: "Emprendimientos", path: "/" },
+    {title: "Favoritos", path: "/" },
+    {title: "Historial", path: "/" },
+]
