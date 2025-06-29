345ad9c5336eb799f77826a3525716d29f741dc8
guilleviera96 <guillefce096@gmail.com>
2025-06-29 13:34:40 -0300

    Se agregaron unos div para acomodar el margen de algunos componentes en home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import { Navbar, CardEmprendimiento, Carrusel } from "../../components";
 import emprendimientos from "../../utils/emprendimientoMock.json";
 function displayEmprendimientos() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 p-3">
       {emprendimientos.map((item, index) => (
         <CardEmprendimiento
           key={index}
@@ -21,8 +21,12 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <Carrusel />
-      {displayEmprendimientos()}
+      <div className="mt-2">
+        <Carrusel />
+      </div>
+      <div className="-mt-10">
+        {displayEmprendimientos()}
+      </div>
     </>
   );
 }
