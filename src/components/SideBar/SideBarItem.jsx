3d1be1d6e91f90c78161454615387f28652b3aaf
FraGal05 <46552826@roquegonzalez.org>
2025-08-25 19:19:26 -0300

    feat: Se configuro el diseño de la lista desplegable

--- src/components/SideBar/SideBarItem.jsx
+++ src/components/SideBar/SideBarItem.jsx
@@ -15,22 +15,26 @@ export const SideBarItem = ({ text, path }) => {
 
 
 export const SideBarDropItem = ({text, items}) => {
-  const [isOpen, SetIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="flex items-center gap-3 px-4 py-2 rounded hover:bg-[#3c56a5] transition-colors">
-      
+    <div className="relative">
       <button
-      onClick={()=>SetIsOpen(!isOpen)}
+      onClick={()=>setIsOpen(!isOpen)} 
+      className="w-full text-left flex items-center gap-3 px-4 py-2 rounded hover:bg-[#3c56a5] cursor-pointer transition-colors"
       >
         <span>{text}</span>
+        <span className="ml-auto transform transition-transform duration-200">
+          {isOpen ? "▼" : "▲"}
+        </span>
       </button>
       
-      <div className={isOpen ? "max-h-40 opacity-100 mb-2" : "max-h-0 opacity-0"}>
-        <ul className="p-4 space-y-2 flex-1">
+      <div className={`absolute bottom-full left-0 w-full transition-all duration-300 ease-in-out${isOpen ? "max-h-40 opacity-100 mb-2" : "max-h-0 opacity-0"}`}>
+        <ul className="p-4 space-y-2 rounded text-[#000] bg-[#fff] flex-1">
           {items.map((item, index) => (
             <li key={index}>
               <Link
               to={item.path}
+              className="flex items-center gap-3 p-2 rounded  hover:bg-[#3c56a5] transition-colors"
               >
                 <span>{item.title}</span>
               </Link>
