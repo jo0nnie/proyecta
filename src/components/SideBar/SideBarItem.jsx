b1887f58a6ce0df7bd601da48181c9bb785fff26
FraGal05 <46552826@roquegonzalez.org>
2025-08-26 21:07:07 -0300

    Se cambio el backgroundcolor (bg) de la lista desplegable

--- src/components/SideBar/SideBarItem.jsx
+++ src/components/SideBar/SideBarItem.jsx
@@ -29,7 +29,7 @@ export const SideBarDropItem = ({text, items}) => {
       </button>
       
       <div className={`absolute bottom-full left-0 w-full transition-all duration-300 ease-in-out${isOpen ? "max-h-40 opacity-100 mb-2" : "max-h-0 opacity-0"}`}>
-        <ul className="p-4 space-y-2 rounded text-[#000] bg-[#fff] flex-1">
+        <ul className="p-4 space-y-2 rounded text-[#000000] bg-[#A9B5EF] flex-1">
           {items.map((item, index) => (
             <li key={index}>
               <Link
