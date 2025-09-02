38313896e987e59bde04333b33b5eebd264c8dec
Lucrecia Galarza <44737561@ROQUEGONZALEZ.ORG>
2025-09-02 12:54:48 -0300

    feat/PRO-63 se creo la pagina de EditarPerfilUsuario y se mergeo el componente button mode la rama PRO-62 para la

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
-export default function Button({ type, text }) {
+export default function Button({ type, text, onClick }) {
   return (
     <div>
     <button
       type={type}
       className="py-2 px-6 bg-primary-500 text-dark-logo text-size-base font-medium rounded-[12px] shadow-md hover:bg-primary-600 transition-all duration-200 mx-auto block font-family-inter cursor-pointer"
+      onClick={onClick}
     >
       {text}
     </button>
