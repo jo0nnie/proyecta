5f11d1037c554e12bf34de6e1b930674602e3ccb
FraGal05 <46552826@roquegonzalez.org>
2025-09-09 20:35:16 -0300

    fix: Se modifico el diseño de la pagina de Historial

--- src/pages/Historial/HistorialScreen.jsx
+++ src/pages/Historial/HistorialScreen.jsx
@@ -4,30 +4,30 @@ import emprendimientos from "../../utils/emprendimientoMock.json";
 export default function HistorialScreen() {
 
   //cree una variable para mostrar, a modo de ejemplo, un solo emprendimiento en la pagina del historial
-  const emprendimientoEjemplo = emprendimientos.find(item => item.id === "1");
+  const emprendimientoEjemplo = emprendimientos.filter(item => item.id > "5");
   return (
     <>
       <div>
-        <h2 className="text-2xl font-bold pl-7 pt-5">Historial</h2>
+        <h1 className="text-3xl md:text-4xl font-bold text-[#2C4692] m-2 p-2 text-center">Historial</h1>
         <div className="ml-7 mr-7 mb-6">
-          <p className="mt-1 text-black">
+          <p className=" text-gray-600 text-center">
             Emprendimientos que has visitado recientemente.
           </p>
           <div className="absolute left-0 right-0 border-b border-[#2C4692] pt-4"></div>
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 p-3">
-        {emprendimientoEjemplo && (
-          <CardEmprendimiento
-            key={emprendimientoEjemplo.id}
-            id={emprendimientoEjemplo.id}
-            nombre={emprendimientoEjemplo.nombre}
-            descripcion={emprendimientoEjemplo.descripcion}
-            categoria={emprendimientoEjemplo.categoria}
-            imagen={emprendimientoEjemplo.imagen}
-          />
-        )}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-8 p-6">
+        {emprendimientoEjemplo.map((item) => (
+              <CardEmprendimiento
+                key={item.id}
+                id={item.id}
+                nombre={item.nombre}
+                descripcion={item.descripcion}
+                categoria={item.categoria}
+                imagen={item.imagen}
+              />
+            ))}
       </div>
     </>
   );
