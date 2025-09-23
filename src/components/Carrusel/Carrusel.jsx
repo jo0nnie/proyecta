a86da7cd29e413f2d082a8a83b48866e48b546c1
guilleviera96 <guillefce096@gmail.com>
2025-09-23 12:12:47 -0300

    fix/PRO-72 Se corrigio el redireccionamiento del componente Carrusel, se agrego una funcion para poder crear ids, se agrego un useEffect en la pagina de categoriasScreen para manejar el comportamiento del scroll durante el redireccionamiento

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -11,12 +11,12 @@ import { MdFavorite } from "react-icons/md";
 //este array lo puse de ejemplo, cuando definamos las categorias vamos a cambiar por sus iconos correspondientes
 
 const categories = [
-  { icon: FaDesktop, label: "Tecnología", path: "/categorias", color: "#2C4692" },
-  { icon: FaTshirt, label: "Moda", path: "/categorias", color: "#2C4692" },
-  { icon: FaHome, label: "Hogar", path: "/categorias", color: "#2C4692" },
-  { icon: FaUtensils, label: "Gastronomía", path: "/categorias", color: "#2C4692" },
-  { icon: MdFavorite, label: "Salud y Bienestar", path: "/categorias", color: "#2C4692" },
-  { icon: FaGift, label: "Diseños y regalos", path: "/categorias", color: "#2C4692" },
+  { icon: FaDesktop, label: "Tecnología", path: "/categorias#tecnologia", color: "#2C4692" },
+  { icon: FaTshirt, label: "Moda", path: "/categorias#moda", color: "#2C4692" },
+  { icon: FaHome, label: "Hogar", path: "/categorias#hogar", color: "#2C4692" },
+  { icon: FaUtensils, label: "Gastronomía", path: "/categorias#gastronomia", color: "#2C4692" },
+  { icon: MdFavorite, label: "Salud y Bienestar", path: "/categorias#salud-y-bienestar", color: "#2C4692" },
+  { icon: FaGift, label: "Diseños y regalos", path: "/categorias#disenos-y-regalos", color: "#2C4692" },
 
 ];
 
